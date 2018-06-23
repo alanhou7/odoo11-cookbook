c518a5d31fd74df05a2b2660cb2253ae6bd80167
alanhou7 <alanhou7@gmail.com>
2018-06-23 09:40:41 -0400

    Chapter 4

---
+++ README.md
@@ -0,0 +1,1 @@
+# odoo11-cookbook
---
+++ my_module/__manifest__.py
@@ -0,0 +1,17 @@
+{
+	'name': 'My Module',
+	'summary': 'My module summary demo',
+	'description': '''Long description demo
+			second line''',
+	'author': 'Alan Hou',
+	'license': "AGPL-3",
+	'website': 'https://alanhou.org',
+	'category': 'Demo Module',
+	'version': '0.1',
+	'application': True,
+	'data': [
+		'views/library_book.xml',
+		'security/groups.xml',
+		'security/ir.model.access.csv'
+	]
+}
